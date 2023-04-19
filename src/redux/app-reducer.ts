511acc01464d0771f7c763ce6f590482840dfd1e
lesterrin <voldemar1810@gmail.com>
2023-04-19 18:45:02 +1000

    app-reducer переведен на TS

--- src/redux/app-reducer.ts
+++ src/redux/app-reducer.ts
@@ -1,24 +1,16 @@
-/*
 import {authMe} from "./auth-reducer";
 
 const INITIALIZED_SUCCESS = 'app/INITIALIZED-SUCCESS';
 
-type InitializedSuccessActionType = {
-    type: typeof INITIALIZED_SUCCESS;
-}
-
-type InitialStateType = {
+export type InitialStateType = {
     isInitialized: boolean
 }
-
-type ReducerType = {};
 
 const initialState: InitialStateType = {
     isInitialized: false
 }
 
-
-const appReducer = (state = initialState, action): ReducerType => {
+const appReducer = (state = initialState, action: any): InitialStateType => {
 
     switch (action.type) {
 
@@ -31,45 +23,13 @@ const appReducer = (state = initialState, action): ReducerType => {
         default:
             return state;
     }
+}
+
+type InitializedSuccessActionType = {
+    type: typeof INITIALIZED_SUCCESS;
 }
 
 export const initializedSuccess = (): InitializedSuccessActionType => ({type: INITIALIZED_SUCCESS});
-
-export const initializeApp = () => (dispatch) => {
-    let promise = dispatch(authMe());
-
-    promise.then(() => {
-        dispatch(initializedSuccess())
-    });
-}
-
-export default appReducer;
-*/
-
-import {authMe} from "./auth-reducer";
-
-const INITIALIZED_SUCCESS = 'app/INITIALIZED-SUCCESS';
-
-const initialState = {
-    isInitialized: false
-}
-
-const appReducer = (state = initialState, action: any) => {
-
-    switch (action.type) {
-
-        case INITIALIZED_SUCCESS:
-            return {
-                ...state,
-                isInitialized: true
-            }
-
-        default:
-            return state;
-    }
-}
-
-export const initializedSuccess = () => ({type: INITIALIZED_SUCCESS});
 
 export const initializeApp = () => (dispatch: any) => {
     let promise = dispatch(authMe());
